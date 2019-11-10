1eb12a759ddfcb28b06b5d2c668b5088af976e35
alanliang0430 <liang.my@aliyun.com>
2019-11-10 22:45:58 +0800

    create a new project

---
+++ spring-framework-custom/src/main/java/com/alan/spring_framework_custom/App.java
@@ -0,0 +1,13 @@
+package com.alan.spring_framework_custom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
